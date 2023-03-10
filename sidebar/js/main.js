62d712e9e4ee1b2ee92d5fd0fef6d69ff8c81a3e
Elshad Rzaguliyev <118837671+ElshadRza@users.noreply.github.com>
2023-03-10 21:35:10 +0400

    Add files via upload

---
+++ sidebar/js/main.js
@@ -0,0 +1,8 @@
+const sidegf =document.querySelector("img");
+const sidenigga=document.querySelector(".sidebar");
+
+
+sidegf.addEventListener("click", ()=>{
+  sidenigga.classList.toggle("sidebar-open");
+  console.log("nigga");
+})
